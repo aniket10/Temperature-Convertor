667f001215780b8195a18e77173e1060e2e084c8
Aniket Ajagaonkar <aniketa@usesoldmove-lm.corp.yahoo.com>
2014-07-26 12:06:35 -0700

    Initial Commit

---
+++ Temperature Converter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Temperature Converter
+//
+//  Created by Aniket Ajagaonkar on 7/26/14.
+//  Copyright (c) 2014 Aniket Ajagaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
